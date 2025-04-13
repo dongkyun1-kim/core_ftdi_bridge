576ae32e2d929d6df3e80c415d19610ad0a53e65
unknown <dongkyun1.kim@sk.com>
2025-04-13 20:59:15 +0900

    new init

--- src_v/ftdi_async.sv
+++ src_v/ftdi_async.sv
@@ -1,34 +1,4 @@
 //-----------------------------------------------------------------
-//                       FTDI FIFO -> AXI Bridge
-//                              V1.0
-//                        Ultra-Embedded.com
-//                        Copyright 2015-2019
-//
-//                 Email: admin@ultra-embedded.com
-//
-//                         License: GPL
-// If you would like a version with a more permissive license for
-// use in closed source commercial applications please contact me
-// for details.
-//-----------------------------------------------------------------
-//
-// This file is open source HDL; you can redistribute it and/or 
-// modify it under the terms of the GNU General Public License as 
-// published by the Free Software Foundation; either version 2 of 
-// the License, or (at your option) any later version.
-//
-// This file is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public 
-// License along with this file; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-// USA
-//-----------------------------------------------------------------
-
-//-----------------------------------------------------------------
 //                          Generated File
 //-----------------------------------------------------------------
 
